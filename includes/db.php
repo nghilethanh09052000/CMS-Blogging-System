8098136edd095b040dffb0d35859d8046a7ce218
thanhnghi0905200 <nghilt19411@gmail.com>
2023-05-12 16:33:49 +0700

    add admin page, add index search page

---
+++ includes/db.php
@@ -0,0 +1,27 @@
+<?php ob_start();
+
+    $db['db_host'] = "localhost:4306";
+    $db['db_user'] = "root";
+    $db['db_pass'] = "";
+    $db['db_name'] = "cms_blogging_website";
+
+    foreach($db as $key => $value)
+    {
+        define(strtoupper($key), $value);
+    }
+
+
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+
+
+    $query = "SET NAMES utf8";
+    mysqli_query($connection, $query);
+
+    // if($connection) 
+    // { 
+    //     echo "We are connected"; 
+    // }
+
+
+?>
